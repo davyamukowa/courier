20315bd4d785cedf85d9c5b2315f496177383677
David Amukowa <davyamukowa@gmail.com>
2026-07-17 23:15:17 +0300

    auto-sync: 2026-07-17 23:15:17

--- modules/courier_goshipping/controllers/Client_quotes.php
+++ modules/courier_goshipping/controllers/Client_quotes.php
@@ -6,6 +6,7 @@ class Client_quotes extends AdminController
     public function __construct()
     {
         parent::__construct();
+        $this->load->helper('courier_goshipping/courier');
         // Since we attached the menu to 'courier-shipments' view_all_shipments in courier.php
         if (!has_permission('courier-shipments', '', 'view_all_shipments') && !is_admin()) {
             access_denied('Courier Client Quotes');
@@ -15,7 +16,7 @@ class Client_quotes extends AdminController
     public function index()
     {
         if ($this->input->is_ajax_request()) {
-            $this->app->get_table_data(module_views_path('courier', 'client_quotes/table'));
+            $this->app->get_table_data(module_views_path('courier_goshipping', 'client_quotes/table'));
         }
         $data['title'] = 'Client Quotes';
         $this->load->view('client_quotes/manage', $data);
