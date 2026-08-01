5095e0ead8e6c0d15dd98712d5b2e697bc00b2fe
David Amukowa <davyamukowa@gmail.com>
2026-08-01 11:23:53 +0300

    auto-sync: 2026-08-01 11:23:52

---
+++ modules/courier_goshipping/libraries/mails/Salibay_shipment_in_transit.php
@@ -0,0 +1,23 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Salibay_shipment_in_transit extends App_mail_template
+{
+    public $slug = 'salibay_shipment_in_transit';
+
+    private $_merge;
+
+    public function __construct($email, array $merge_fields = [])
+    {
+        parent::__construct();
+        $this->send_to = $email;
+        $this->_merge  = $merge_fields;
+    }
+
+    public function build()
+    {
+        $this->to($this->send_to)
+             ->set_merge_fields($this->_merge);
+    }
+}
